60417bf33d2bc104ae583075df04bb32b4420863
Kian Seyedjafari <kian.matthew@berkeley.edu>
2023-03-10 11:24:55 -0800

    tested product seeder

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .product import seed_products, undo_products
 
 from app.models.db import db, environment, SCHEMA
 
@@ -12,12 +13,14 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
-        # Before seeding in production, you want to run the seed undo 
-        # command, which will  truncate all tables prefixed with 
+        # Before seeding in production, you want to run the seed undo
+        # command, which will  truncate all tables prefixed with
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
+        undo_products()
     seed_users()
+    seed_products()
     # Add other seed functions here
 
 
@@ -25,4 +28,5 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_products()
     # Add other undo functions here
