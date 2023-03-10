0b55211cf830c0edf51da062c4e386bd473e8f1a
Leandro <leandro.figueiredo14848@gmail.com>
2023-03-10 16:02:46 -0500

    initial component file

---
+++ react-app/src/components/Products/index.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react'
+import {useDispatch, useSelector} from "react-redux"
+import { NavLink, Switch, Route } from 'react-router-dom'
+import './Products.css'
+
+function AllProducts() {
+
+    return (
+        <div>
+            <h1>TEST RENDER ALL LISTINGS</h1>
+        </div>
+    )
+}
+
+export default AllProducts;
---
+++ react-app/src/components/Products/singleProduct/index.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import {useParams, useHistory} from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux";
+import './singleProduct.css'
+
+function Product () {
+
+    return (
+        <div>
+            <h1>SINGLE PRODUCT TEST RENDER</h1>
+        </div>
+    )
+}
+
+export default Product
