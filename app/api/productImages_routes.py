67c333e74f8d88dd90f4fd61bb357c01e0e27c34
Kian Seyedjafari <kian.matthew@berkeley.edu>
2023-03-11 12:52:14 -0800

    simple product images routes

---
+++ app/api/productImages_routes.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, jsonify
+from app.models import Product, db
+
+productImages_routes = Blueprint('productImages', __name__)
+
+
+@productImages_routes.route('/')
+def allImages():
+    images = ProductImages.query.all()
+    return {'productImages': [images.to_dict() for image in images]}
+
+@productImages_routes.route('/<int:id>')
+def imagesById(id):
+    images = ProductImages.query.get(id)
+    print("IMAGES", images.to_dict())
+    return images.to_dict()
