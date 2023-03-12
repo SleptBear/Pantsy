4d1bc9fd9f13e8ee7b94d93a888097ce81642305
sfazli96 <sfazli96@gmail.com>
2023-03-11 22:48:10 -0800

    create product-wtforms success

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,15 +1,17 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models import Product, db
+from app.forms import ProductForm
 
 product_routes = Blueprint('products', __name__)
 
 
-
+# gets all the products
 @product_routes.route('/')
 def allProducts():
     products = Product.query.all()
     return {'products': [product.to_dict() for product in products]}
 
+# Gets the product by id
 @product_routes.route('/<int:id>')
 def singleProduct(id):
     product = Product.query.get(id)
@@ -24,11 +26,31 @@ def singleProduct(id):
     pd.update(pdImages)
     return pd
 
-# @product_routes.route('/', methods=['POST'])
-# def createProduct(id):
+# Creates a product
+@product_routes.route('/', methods=['POST'])
+def createProduct():
+    data = request.get_json()
+    form = ProductForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_product = Product(
+            name = data["name"],
+            description = data["description"],
+            price = str(data["price"]),
+            seller = data["seller"],
+            category = data["category"],
+            color = data["color"],
+            size = data["size"]
+        )
+        print(new_product.to_dict())
+        db.session.add(new_product)
+        db.session.commit()
 
+        return new_product.to_dict()
+    else:
+        return "Bad Data"
 
-
+# Delete a product by id
 @product_routes.route('/<int:id>', methods=['DELETE'])
 def removeProduct(id):
     product = Product.query.get(id)
