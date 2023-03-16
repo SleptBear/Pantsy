1009ef355cbfc406e8cf9b4093ef60dae3c40ea3
Vian <viankhachatourian@gmail.com>
2023-03-16 14:54:49 -0700

    updates to cart

--- react-app/src/components/Cart/index.js
+++ react-app/src/components/Cart/index.js
@@ -6,11 +6,14 @@ import { deleteCartThunk, loadCartThunk } from '../../store/cart'
 const Cart = () => {
     const dispatch = useDispatch()
     const user = useSelector(state => state.session.user)
-    const products = useSelector(state => state.cartReducer.Cart.products)
-    console.log("products", products)
+    const products = useSelector(state => state.cartReducer.Cart)
     useEffect(() => {
         dispatch(loadCartThunk(user.id))
     }, [dispatch])
+
+    // const productsArr = Object.values(products) || []
+    console.log("Products", products)
+    // console.log("productsArr", productsArr)
 
     if(!products) {
         return <h1>You have no items in your cart</h1>
@@ -18,16 +21,19 @@ const Cart = () => {
     return (
         <div>
             <h1>TEST</h1>
-            {products.map(({id, name, price, size, color}) => {
+            {/* {productsArr?.map(({id, name, price, size, color, productimages}) => {
                 return <div>
                     <h1>Item</h1>
                     <p>{name}</p>
                     <p>{price}</p>
                     <p>{size}</p>
                     <p>{color}</p>
-                    {/* <button onClick={dispatch(deleteCartThunk(id))}>Delete</button> */}
+                    {productimages?.map(image => {
+                        <img src={image} alt="no image"></img>
+                    })}
                 </div>
-            })}
+            })} */}
+            {/* <button onClick={dispatch(deleteCartThunk(id))}>Delete</button> */}
         </div>
     )
 }
