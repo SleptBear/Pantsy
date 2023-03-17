bbce15539e515e9a3daa25cad1a6cc0c527a55b5
sfazli96 <sfazli96@gmail.com>
2023-03-17 11:29:04 -0700

    fixing order backend

--- react-app/src/components/Order/index.js
+++ react-app/src/components/Order/index.js
@@ -7,6 +7,7 @@ const Order = () => {
     const dispatch = useDispatch();
     const user = useSelector((state) => state.session.user);
     const orders = useSelector((state) => state.orderReducer.orders);
+    console.log('ORDERs', orders)
 
     useEffect(() => {
       if (user && user.id) {
@@ -18,21 +19,10 @@ const Order = () => {
       return <h1>You have no orders</h1>;
     }
 
-    const totalPrice = orders.reduce((total, order) => {
-      const orderPrice = order.total_price ? Number(order.total_price) : order.products.reduce((orderTotal, product) => {
-        const productPrice = Number(product.price);
-        return orderTotal + (isNaN(productPrice) ? 0 : productPrice)
-      }, 0);
-
-      return total + (isNaN(orderPrice) ? 0 : orderPrice)
-    }, 0);
-
 
     return (
       <div>
         <h1>Orders</h1>
-        <table>
-          <tbody>
           {orders.map((order) => (
           <tr key={order.id}>
             <td>Order Date: {order.date}</td>
@@ -41,14 +31,8 @@ const Order = () => {
             ))}
           </tr>
         ))}
-          </tbody>
-          <tfoot>
-            <tr>
-              <td>Total Spent so far:</td>
-              <td>{totalPrice}</td>
-            </tr>
-          </tfoot>
-        </table>
+          {/* <td>Total Spent so far:</td>
+          <td>{totalPrice}</td> */}
       </div>
     );
   };
