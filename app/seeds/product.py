b73fc47601221dcd8b89a7184efef86347452d96
Leandro <leandro.figueiredo14848@gmail.com>
2023-03-19 19:52:48 -0400

    more seeders, allproducts css, and add img validation fix

--- app/seeds/product.py
+++ app/seeds/product.py
@@ -3,41 +3,77 @@ from sqlalchemy.sql import text
 
 def seed_products():
     demo1 = Product(
-        name="Jeans", description="Sleek looking Blue jeans", price="12.99", seller=1, category="jeans", color='Blue', size='Small'
+        name="Twill Chinos", description="Sleek looking Beige chinos in Small size", price="45.99", seller=1, category="Chinos", color='Beige', size='Small'
     )
     demo2 = Product(
-        name="Jeans", description="Sleek looking Red jeans", price="12.99", seller=1, category="jeans", color='Red', size='Medium'
+        name="Twill Chinos", description="Sleek looking Navy chinos in Medium size", price="45.99", seller=1, category="Chinos", color='Navy', size='Medium'
     )
     demo3 = Product(
-        name="Jeans", description="Sleek looking Black jeans", price="12.99", seller=2, category="jeans", color='Black', size='Large'
+        name="Twill Chinos", description="Sleek looking Black chinos in Large size", price="45.99", seller=2, category="Chinos", color='Black', size='Large'
     )
     demo4 = Product(
-        name="Slacks", description="Perfect Black slacks for a Casual setting", price="15.99", seller=2, category="casual", color='Black', size='Small'
+        name="Mens Adicross Chino Pants", description="Unique Black chinos for outdoor wear, in Small size", price="15.99", seller=2, category="Chinos", color='Black', size='Small'
     )
     demo5 = Product(
-        name="Slacks", description="Perfect White slacks for a Casual setting", price="15.99", seller=3, category="casual", color='White', size='Medium'
+        name="Mens Adicross Chino Pants", description="Unique Grey chinos for outdoor wear, in Medium size", price="15.99", seller=3, category="Chinos", color='Grey', size='Medium'
     )
     demo6 = Product(
-        name="Slacks", description="Perfect Teal slacks for a Casual setting", price="15.99", seller=4, category="casual", color='Teal', size='Large'
+        name="Mens Adicross Chino Pants", description="Unique Alumina chinos for casual wear, in Large size", price="15.99", seller=4, category="Chinos", color='Alumina', size='Large'
     )
     demo7 = Product(
-        name="Khakis", description="These Beige khakis will make your work uniform look great and your job less miserable!", price="19.99", seller=3, category="pants", color='Beige', size='Small'
+        name="Relaxed Jeans", description="These Beige relaxed jeans will make your work uniform look great and your job less miserable!", price="19.99", seller=3, category="jeans", color='Beige', size='Small'
     )
     demo8 = Product(
-        name="Khakis", description="These Black khakis will make your work uniform look great and your job less miserable!", price="19.99", seller=4, category="pants", color='Black', size='Medium'
+        name="Relaxed Jeans", description="These Beige relaxed jeans will make your work uniform look great and your job less miserable!", price="19.99", seller=4, category="jeans", color='Beige', size='Medium'
     )
     demo9 = Product(
-        name="Khakis", description="These Grey khakis will make your work uniform look great and your job less miserable!", price="19.99", seller=1, category="pants", color='Grey', size='Large'
+        name="Relaxed Jeans", description="These Beige relaxed jeans will make your work uniform look great and your job less miserable!", price="19.99", seller=1, category="jeans", color='Beige', size='Large'
     )
     demo10 = Product(
-        name="Joggers", description="Unique joggers, can be worn as a fashion statement or to workout!", price="9.99", seller=2, category="jogger", color='Green', size='Small'
+        name="Green Pants", description="Generic green pants, dirt cheap will shrink after 1 wash", price="6.99", seller=2, category="jogger", color='Green', size='Small'
     )
     demo11 = Product(
-        name="Joggers", description="Unique joggers, can be worn as a fashion statement or to workout!", price="9.99", seller=3, category="jogger", color='Black', size='Medium'
+        name="Green Pants", description="Generic green pants, dirt cheap will shrink after 1 wash", price="6.99", seller=3, category="jogger", color='Black', size='Medium'
     )
     demo12 = Product(
-        name="Joggers", description="Unique joggers, can be worn as a fashion statement or to workout!", price="9.99", seller=4, category="jogger", color='Grey', size='Large'
+        name="Green Pants", description="Generic green pants, dirt cheap will shrink after 1 wash", price="6.99", seller=4, category="jogger", color='Grey', size='Large'
     )
+    demo13 = Product(
+        name="Cargo Pants", description="you would think there would be more pockets in these", price="24.99", seller=2, category="cargo", color='green', size='small'
+    )
+    demo14 = Product(
+        name="Beige Cargo pants Pants", description="you would think there would be more pockets in these", price="24.99", seller=2, category="cargo", color='beige', size='small'
+    )
+    demo15 = Product(
+        name="High Waisted Dress Pants", description="High waisted pleated pants perfect for business and casual setting", price="85.99", seller=3, category="dress", color='Grey', size='Small'
+    )
+    demo16 = Product(
+        name="High Waisted Dress Pants", description="High waisted pleated pants perfect for business and casual setting", price="85.99", seller=3, category="dress", color='Grey', size='Small'
+    )
+    demo17 = Product(
+        name="High Waisted Dress Pants", description="High waisted pleated pants perfect for business and casual setting", price="85.99", seller=3, category="dress", color='Grey', size='Small'
+    )
+    demo18 = Product(
+        name="Yellow Pants", description="This is just a gag item, please dont wear it in public setting", price="23.99", seller=1, category="jeans", color='yellow', size='Small'
+    )
+    demo19 = Product(
+        name="Yellow Pants", description="This is just a gag item, please dont wear it in public setting", price="23.99", seller=1, category="jeans", color='yellow', size='Medium'
+    )
+    demo20 = Product(
+        name="Yellow Pants", description="This is just a gag item, please dont wear it in public setting", price="23.99", seller=1, category="jeans", color='yellow', size='Large'
+    )
+    demo21 = Product(
+        name="Just buy shorts instead", description="Buy this so you can finish converting these into shorts", price="99.99", seller=2, category="jeans", color='blue', size='Small'
+    )
+    demo22 = Product(
+        name="Just buy shorts instead", description="Buy this so you can finish converting these into shorts", price="99.99", seller=2, category="jeans", color='blue', size='Medium'
+    )
+    demo23 = Product(
+        name="Just buy shorts instead", description="Buy this so you can finish converting these into shorts", price="99.99", seller=2, category="jeans", color='blue', size='Large'
+    )
+    # demo20 = Product(
+    #     name="", description="", price="", seller=4, category="", color='', size=''
+    # )
 
     db.session.add(demo1)
     db.session.add(demo2)
@@ -51,6 +87,17 @@ def seed_products():
     db.session.add(demo10)
     db.session.add(demo11)
     db.session.add(demo12)
+    db.session.add(demo13)
+    db.session.add(demo14)
+    db.session.add(demo15)
+    db.session.add(demo16)
+    db.session.add(demo17)
+    db.session.add(demo18)
+    db.session.add(demo19)
+    db.session.add(demo20)
+    db.session.add(demo21)
+    db.session.add(demo22)
+    db.session.add(demo23)
     db.session.commit()
 
 
