01adb058316db5b6e8876e32baed598ef9a5ff54
sfazli96 <sfazli96@gmail.com>
2023-04-25 15:49:32 -0700

    changed edit product input field to dropdown options

--- react-app/src/components/Products/addProduct/index.js
+++ react-app/src/components/Products/addProduct/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import {useDispatch, useSelector} from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { NavLink, Switch, Route, useHistory } from 'react-router-dom'
 import { createProductThunk } from '../../../store/product'
 import "./addproduct.css"
@@ -37,8 +37,8 @@ const AddProduct = () => {
         preview: true
     }
 
-    if(!user) {
-       return <h4>User not logged in</h4>
+    if (!user) {
+        return <h4>User not logged in</h4>
     }
 
     const handleSubmit = async (e) => {
@@ -114,81 +114,81 @@ const AddProduct = () => {
         //     console.log("error");
         // }
 
-              //     const response = await fetch(image, { method: 'HEAD' });
-              //     const contentType = response.headers.get('content-type');
-              //     if (!contentType || !contentType.startsWith('image/')) {
-              //         setErrors(errors => [...errors, 'Please enter a valid image link']);
-              //         return;
-              //     }
+        //     const response = await fetch(image, { method: 'HEAD' });
+        //     const contentType = response.headers.get('content-type');
+        //     if (!contentType || !contentType.startsWith('image/')) {
+        //         setErrors(errors => [...errors, 'Please enter a valid image link']);
+        //         return;
+        //     }
 
-        dispatch(createProductThunk({ProductData, imgData}))
-        .then(() => history.push("/"))
-        .catch(async (res) => {
-            const data = await res.json();
-            if (data && data.errors) setErrors(data.errors)
-          });
+        dispatch(createProductThunk({ ProductData, imgData }))
+            .then(() => history.push("/"))
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors)
+            });
     }
 
     const updateImage = (e) => {
         const file = e.target.files[0];
         setImage(file);
     }
-    return(
-        <div className= "addproductmain">
+    return (
+        <div className="addproductmain">
 
             <form className="addproductform" onSubmit={handleSubmit} noValidate>
                 <h1>Add a Product</h1>
                 <ul className="error-message">
-                {errors.map((error, idx) => (
-                <li key={idx} className="error-text">
-                    {error}
-                </li>
-                ))}
+                    {errors.map((error, idx) => (
+                        <li key={idx} className="error-text">
+                            {error}
+                        </li>
+                    ))}
                 </ul>
-            <label className="namelabel">
-                Name
-            <input className="name-form"
-            type="text"
-            value={name}
-            placeholder="Name"
-            maxLength={50}
-            onChange={(e) => {
-                setName(e.target.value)
-            }}
-            required
+                <label className="namelabel">
+                    Name
+                    <input className="name-form"
+                        type="text"
+                        value={name}
+                        placeholder="Name"
+                        maxLength={50}
+                        onChange={(e) => {
+                            setName(e.target.value)
+                        }}
+                        required
 
-            ></input>
-            </label>
-            <label className="descriptionlabel">
-                Description
-            <textarea className="description-form"
+                    ></input>
+                </label>
+                <label className="descriptionlabel">
+                    Description
+                    <textarea className="description-form"
 
-            value={description}
+                        value={description}
 
-            placeholder="Describe your product here"
-            maxLength={255}
-            onChange={(e) => {
-                setDescription(e.target.value)
-            }}
-            required
+                        placeholder="Describe your product here"
+                        maxLength={255}
+                        onChange={(e) => {
+                            setDescription(e.target.value)
+                        }}
+                        required
 
-            ></textarea>
-            </label>
-            <label className="pricelabel">
-                Price
-            <input className="price-form"
-            type="text"
-            value={stringprice}
-            placeholder="Price"
-            maxLength={50}
-            onChange={(e) => {
-                setstringPrice(e.target.value)
-            }}
-            required
+                    ></textarea>
+                </label>
+                <label className="pricelabel">
+                    Price
+                    <input className="price-form"
+                        type="text"
+                        value={stringprice}
+                        placeholder="Price"
+                        maxLength={50}
+                        onChange={(e) => {
+                            setstringPrice(e.target.value)
+                        }}
+                        required
 
-            ></input>
-            </label>
-            {/* <label className="categorylabel">
+                    ></input>
+                </label>
+                {/* <label className="categorylabel">
                 Category
             <input className="category-form"
             type="text"
@@ -202,28 +202,28 @@ const AddProduct = () => {
 
             ></input>
             </label> */}
-            <label className="categorylabel">
-                Category
-                <select
-                    className="category-form"
-                    value={category}
-                    onChange={(e) => {
-                        setCategory(e.target.value)
-                    }}
-                    required
-                >
-                    <option value="">Select a category</option>
-                    <option value="slacks">Slacks</option>
-                    <option value="jeans">Jeans</option>
-                    <option value="trousers">Trousers</option>
-                    <option value="leggings">Leggings</option>
-                    <option value="shorts">Shorts</option>
-                    <option value="cargo pants">Cargo Pants</option>
-                    <option value="khakis">Khakis</option>
-                </select>
-            </label>
+                <label className="categorylabel">
+                    Category
+                    <select
+                        className="category-form"
+                        value={category}
+                        onChange={(e) => {
+                            setCategory(e.target.value)
+                        }}
+                        required
+                    >
+                        <option value="">Select a category</option>
+                        <option value="slacks">Slacks</option>
+                        <option value="jeans">Jeans</option>
+                        <option value="trousers">Trousers</option>
+                        <option value="leggings">Leggings</option>
+                        <option value="shorts">Shorts</option>
+                        <option value="cargo pants">Cargo Pants</option>
+                        <option value="khakis">Khakis</option>
+                    </select>
+                </label>
 
-            {/* <label className="colorlabel">
+                {/* <label className="colorlabel">
                 Color
             <input className="color-form"
             type="text"
@@ -237,24 +237,24 @@ const AddProduct = () => {
 
             ></input>
             </label> */}
-            <label className="colorlabel">
-                Color
-                <select className="color-form" value={color} onChange={(e) => setColor(e.target.value)}>
-                    <option value="">Select a color</option>
-                    <option value="red">Red</option>
-                    <option value="blue">Blue</option>
-                    <option value="green">Green</option>
-                    <option value="yellow">Yellow</option>
-                    <option value="orange">Orange</option>
-                    <option value="purple">Purple</option>
-                    <option value="pink">Pink</option>
-                    <option value="brown">Brown</option>
-                    <option value="gray">Gray</option>
-                    <option value="black">Black</option>
-                    <option value="white">White</option>
-                </select>
-            </label>
-            {/* <label className="sizelabel">
+                <label className="colorlabel">
+                    Color
+                    <select className="color-form" value={color} onChange={(e) => setColor(e.target.value)}>
+                        <option value="">Select a color</option>
+                        <option value="red">Red</option>
+                        <option value="blue">Blue</option>
+                        <option value="green">Green</option>
+                        <option value="yellow">Yellow</option>
+                        <option value="orange">Orange</option>
+                        <option value="purple">Purple</option>
+                        <option value="pink">Pink</option>
+                        <option value="brown">Brown</option>
+                        <option value="gray">Gray</option>
+                        <option value="black">Black</option>
+                        <option value="white">White</option>
+                    </select>
+                </label>
+                {/* <label className="sizelabel">
                 Size
             <input className="size-form"
             type="text"
@@ -268,23 +268,23 @@ const AddProduct = () => {
 
             ></input>
             </label> */}
-            <label className="sizelabel">
-                Size
-            <select className="size-form"
-                value={size}
-                onChange={(e) => {
-                setSize(e.target.value)
-                }}
-                required
-            >
-                <option value="">Select Size</option>
-                <option value="Small">Small</option>
-                <option value="Medium">Medium</option>
-                <option value="Large">Large</option>
-            </select>
-            </label>
+                <label className="sizelabel">
+                    Size
+                    <select className="size-form"
+                        value={size}
+                        onChange={(e) => {
+                            setSize(e.target.value)
+                        }}
+                        required
+                    >
+                        <option value="">Select Size</option>
+                        <option value="Small">Small</option>
+                        <option value="Medium">Medium</option>
+                        <option value="Large">Large</option>
+                    </select>
+                </label>
 
-            {/* <label className="imagelabel">
+                {/* <label className="imagelabel">
                 Image
             <input className="size-form"
             type="url"
@@ -297,16 +297,16 @@ const AddProduct = () => {
 
             ></input>
             </label> */}
-            <label className="imagelabel">
-            Image
-            <input
-              type="file"
-              accept="image/*"
-              onChange={updateImage}
-            />
-            </label>
-            <button className="submit-form" type="Submit" >Submit</button>
-            {/* {imageLoading && <p>Loading...</p>} */}
+                <label className="imagelabel">
+                    Image
+                    <input
+                        type="file"
+                        accept="image/*"
+                        onChange={updateImage}
+                    />
+                </label>
+                <button className="submit-form" type="Submit" >Submit</button>
+                {/* {imageLoading && <p>Loading...</p>} */}
             </form>
             {/* <button className="demo-add-item"
             onClick={() => dispatch(createProductThunk(({name: "Demo Pants",description: "This is a description",price: 19.99, category: "pants" , color: "Demo Color", size: "Demo Size", seller: user?.id},
