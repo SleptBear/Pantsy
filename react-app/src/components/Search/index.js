a807b670edd68f81bb23b87c810c189089a790af
sfazli96 <sfazli96@gmail.com>
2023-05-08 14:33:44 -0700

    search results shows images with results

--- react-app/src/components/Search/index.js
+++ react-app/src/components/Search/index.js
@@ -7,6 +7,8 @@ import './search.css'
 const Search = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const searchResult = useSelector(state => state.searchReducer);
+  const products = useSelector(state => state.productsReducer.allProducts)
+  const productsArr = Object.values(products || [])
   const dispatch = useDispatch();
 
   const handleSearch = async () => {
@@ -16,18 +18,25 @@ const Search = () => {
   return (
     <div className="search-results">
       {searchResult && Object.keys(searchResult).length > 0 ? (
-        Object.values(searchResult).map((product) => (
-          <div key={product.id} className="search-result">
-            <Link to={`/products/${product.id}`}>
-              {/* <div className="search-result-image" style={{ backgroundImage: `url(${product.image})` }}></div> */}
-              <div className="search-result-details">
-                <h3 className="search-result-title">{product.name}</h3>
-                <p className="search-result-description">{product.description}</p>
-                <p className="search-result-price">{product.price}</p>
-              </div>
-            </Link>
-          </div>
-        ))
+        Object.values(searchResult).map((product) => {
+          // Find the product image from the products array
+          const foundProduct = productsArr.find(p => p.id === product.id);
+          const productImage = foundProduct && foundProduct.productImages && foundProduct.productImages.length > 0 ? foundProduct.productImages[0].image : null;
+
+          return (
+            <div key={product.id} className="search-result">
+              <Link to={`/products/${product.id}`}>
+                {/* Display the product image */}
+                {productImage && <div className="search-result-image" style={{ backgroundImage: `url(${productImage})` }}></div>}
+                <div className="search-result-details">
+                  <h3 className="search-result-title">{product.name}</h3>
+                  <p className="search-result-description">{product.description}</p>
+                  <p className="search-result-price">{product.price}</p>
+                </div>
+              </Link>
+            </div>
+          );
+        })
       ) : (
         <p>No search results found.</p>
       )}
